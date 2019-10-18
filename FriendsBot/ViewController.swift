ed32e6e1b326f4b10d45f582d8d0e6e8e041dcb6
Matheus Alano <matheus_dias@terceiros.sicredi.com.br>
2019-10-18 20:14:47 -0300

    Project created

---
+++ FriendsBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendsBot
+//
+//  Created by Matheus Alano on 18/10/19.
+//  Copyright © 2019 Matheus Alano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
